63c2dddb2b113df6e40622fd9e4c84cfc3ab1ed1
“annasantannaarantes” <anna_luiza_santanna_arantes@brown.edu>
2024-02-16 10:42:40 -0500

    fix: added broadband endpoint

--- src/main/java/edu/brown/cs/student/main/server/Server.java
+++ src/main/java/edu/brown/cs/student/main/server/Server.java
@@ -1,4 +1,10 @@
 package edu.brown.cs.student.main.server;
+import edu.brown.cs.student.main.server.handler.LoadCSVHandler;
+import edu.brown.cs.student.main.server.handler.SearchCSVHandler;
+import edu.brown.cs.student.main.server.handler.ViewCSVHandler;
+import edu.brown.cs.student.main.server.acs.BroadbandHandler;
+import edu.brown.cs.student.main.server.acs.ACSAPI;
+
 import spark.Spark;
 import static spark.Spark.after;
 
@@ -10,35 +16,21 @@ public class Server {
     public static void main(String[] args) {
         int port = 2412;
         Spark.port(port);
-    /*
-       Setting CORS headers to allow cross-origin requests from the client; this is necessary for the client to
-       be able to make requests to the server.
 
-       By setting the Access-Control-Allow-Origin header to "*", we allow requests from any origin.
-       This is not a good idea in real-world applications, since it opens up your server to cross-origin requests
-       from any website. Instead, you should set this header to the origin of your client, or a list of origins
-       that you trust.
-
-       By setting the Access-Control-Allow-Methods header to "*", we allow requests with any HTTP method.
-       Again, it's generally better to be more specific here and only allow the methods you need, but for
-       this demo we'll allow all methods.
-
-       We recommend you learn more about CORS with these resources:
-           - https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-           - https://portswigger.net/web-security/cors
-    */
         after(
                 (request, response) -> {
                     response.header("Access-Control-Allow-Origin", "*");
                     response.header("Access-Control-Allow-Methods", "*");
                 });
 
-        // Setting up the handler for the GET /order and /activity endpoints
+        // Setting up the handler for the GET endpoints
         LoadCSVHandler loadHandler = new LoadCSVHandler();
 
         Spark.get("loadCSV", loadHandler);
         Spark.get("viewCSV", new ViewCSVHandler(loadHandler));
         Spark.get("searchCSV", new SearchCSVHandler(loadHandler));
+        Spark.get("broadband", new BroadbandHandler(new ACSAPI(), true));
+
         Spark.init();
         Spark.awaitInitialization();
 
